718e61437eeae9bc4c86f4a8f9e0cf3fda2178a4
sayumio <s83.oshima.sayumi@gmail.com>
2020-10-06 15:59:39 +0900

    訂正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,17 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock 3.14.1
+lock '3.14.1'
 
 # Capistranoのログの表示に利用する
-set :application, furima-29268
+set :application, 'furima-29268'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  git@github.com:sayumio/furima-29268.git
+set :repo_url, 'git@github.com:sayumio/furima-29268.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, 2.6.5
+set :rbenv_ruby, '2.6.5'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
